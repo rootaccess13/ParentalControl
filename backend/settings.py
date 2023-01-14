029aa0dbb47c3223028f389688f7c0f08c54461e
Mave Rick Tena <mavericktena@Mave.local>
2023-01-14 17:47:57 +0800

    Dependencies

--- backend/settings.py
+++ backend/settings.py
@@ -171,9 +171,9 @@ CORS_ALLOW_METHODS = [
 CORS_ORIGIN_ALLOW_ALL = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
