8cf08e344525daf4a0f2e54fb8619c88a881c3be
Mave Rick Tena <mavericktena@Mave.local>
2023-01-15 23:48:59 +0800

    Messages Framework

--- index/views.py
+++ index/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from api.models import Profile, UrlType, URLBlacklist, ReportURL
+import re
+from django.contrib import messages
 
 def index(request):
     return render(request, 'api/index.html')
@@ -13,8 +15,18 @@ def dashboard(request):
 def reporter(request):
     if request.method == 'POST':
         url = request.POST.get('url')
+        if not url:
+            messages.error(request, "No url provided.")
+            return redirect('reporter')
+        if not re.match(r'^https?://', url):
+            messages.error(request, "Invalid url format")
+            return redirect('reporter')
+        if ReportURL.objects.filter(url=url).exists():
+            messages.error(request, "Url already exists.")
+            return redirect('reporter')
         report_url = ReportURL(url=url)
         report_url.save()
+        messages.success(request, "URL successfully submitted.")
         return redirect('reporter')
     context = {
         'report_list': ReportURL.objects.all()
